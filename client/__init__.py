b55124d49f3f39124498d51005b2996ebd5d8496
Doug Chapman <prjctgeek@gmail.com>
2022-05-29 10:57:29 -0700

    Add some comments

--- client/__init__.py
+++ client/__init__.py
@@ -2,13 +2,13 @@
 import boto3
 import ipaddress
 import configparser
+from time import sleep
 from pysnmp.hlapi import *
+from botocore.exceptions import ClientError
 
-
-class SnmpCloudWatchErr:
-    pass
 
 class Config:
+
     client = {}
 
     def __init__(self, file_name):
@@ -20,6 +20,7 @@ class Config:
         self.client['community'] = self.config['general']['community']
         self.client['hostname'] = self.config['client']['hostname']
         self.client['aws_region'] = self.config['client']['aws_region']
+        self.client['interface'] = self.config['client']['interface']
         self.client['ipaddress'] = self.config['client']['ip']
         self.client['oid_rx'] = self.config['client']['oid_rx']
         self.client['oid_tx'] = self.config['client']['oid_tx']
@@ -30,6 +31,10 @@ class Config:
 
 
 class Result:
+    """
+    Result object from PySNMP
+    with the hostname injected in (we could just poll for that too?).
+    """
     def __init__(self, hostname, tr):
         (error_indication, error_status, error_index, var_binds) = tr
         self._data={"hostname": hostname,
@@ -41,9 +46,11 @@ class Result:
     def get_value_by_oid(self, oid):
         """
         The snmp library is converting the numerical oid into a partial
-        human readible, which screws up the pattern matching.
+        human readable, which screws up the pattern matching.
         pull out a fragment of the oid to match.
-        :param oid:
+        Reminder that the var_binds SNMP result can contain many, many results.
+
+        :param oid: numeric string
         :return:
         """
         partial = ".".join(oid.split(".")[6:-1])
@@ -52,18 +59,49 @@ class Result:
                 return str(response[1])
 
     def get_hostname(self):
+        """
+        Retrive the hostname we added to the result.
+        this could be queries and pulled back out with the get_value_by_oid?
+        :return:
+        """
         return self._data['hostname']
 
 
 class Client:
     results = {}
+
     def __init__(self, config:Config):
         self.config = config
 
-    def once(self):
+    def once(self, put_data_cb, boto_client):
+        """
+        Run one SNMP query, use the put_data_cb to give the response back to the caller.
+        We don't manage boto in the library; expecting a boto  client object.
+        :param put_data_cb:
+        :param boto_client:
+        :return:
+        """
         self.query()
+        return put_data_cb(boto_client, self.get_data())
+
+    def poll(self, put_data_cb, boto_client, wait_in_sec):
+        """
+        Repeatdly call query
+        :param put_data_cb:
+        :param boto_client:
+        :param wait_in_sec:
+        :return:
+        """
+        while True:
+            self.query()
+            put_data_cb(boto_client, self.get_data())
+            sleep(wait_in_sec)
 
     def query(self):
+        """
+        Run one SNMP query, store in a Result object
+        :return:
+        """
         iterator = getCmd(
             SnmpEngine(),
             CommunityData(self.config.client['community'], mpModel=0),
@@ -71,14 +109,20 @@ class Client:
             ContextData(),
             ObjectType(ObjectIdentity(self.config.client['oid_tx'])),
             ObjectType(ObjectIdentity(self.config.client['oid_rx'])),
+            ObjectType(ObjectIdentity(self.config.client['interface'])),
         )
 
-        self.results=Result(self.config.client['hostname'],next(iterator))
-        #print(self.results.get_value_by_oid(self.config.client['oid_tx']))
+        self.results=Result(self.config.client['hostname'], next(iterator))
+        logging.debug(self.results.get_value_by_oid(self.config.client['oid_tx']))
 
     def get_data(self):
+        """
+        Combine the SNMP Result object and configuration settings into something we can use.
+        :return:
+        """
+        # TODO: double check that these are really bytes and not MB.  No MIB available from Unifi to convfirm!
         return {"hostname": self.results.get_hostname(),
+                "interface": self.results.get_value_by_oid(self.config.client['interface']),
                 "rx_mbytes": self.results.get_value_by_oid(self.config.client['oid_rx']),
                 "tx_mbytes": self.results.get_value_by_oid(self.config.client['oid_tx'])}
 
-    # The tx number is sooo munch higher, double check that these are in the right format - they both should be the samew...
--- snmpcw.py
+++ snmpcw.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
-
-import boto3
 import click
 import logging
 from client import *
-from botocore.exceptions import ClientError
 
 # logger config
 logger = logging.getLogger()
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
 
 def put_cw_data(boto_client, data):
+    """
+    Callback given to the SNMPCW Client
+    :param boto_client: boto client object
+    :param data:
+    :return:
+    """
     logging.debug(f"Pushing metrics to CW: {data}")
     response = boto_client.put_metric_data(
         Namespace='SNMP data',
         MetricData=[
             {
-                'MetricName': 'TransmissionBytes',
+                'MetricName': '{}TransmissionBytes'.format(data['interface']),
                 'Dimensions':[
                     {
                         'Name': 'hostname',
@@ -27,31 +30,64 @@ def put_cw_data(boto_client, data):
                 ],
                 'Value': int(data['rx_mbytes']),
                 'Unit': 'Bytes'
-            }
+            },
+            {
+                'MetricName': '{}RecievedBytes'.format(data['interface']),
+                'Dimensions': [
+                    {
+                        'Name': 'hostname',
+                        'Value': data['hostname']
+                    }
+                ],
+                'Value': int(data['tx_mbytes']),
+                'Unit': 'Bytes'
+            },
         ]
     )
+    if '200' not in response:
+        logging.warning(response)
     return response
 
-def load_config():
-    pass
 
 @click.group()
 def query():
-    """ Run one snmp polling cycle, then exit"""
+    """ Run snmp poll, and send to CloudWatch. """
+
 
 @query.command()
 def once():
-    print("Snmp CloudWatch starting")
+    """
+    Run one snmp poll/CW update and exit.
+    :return:
+    """
+    print("Snmp CloudWatch starting in one-shot mode")
     config = Config("sample.config")
     client = Client(config)
-    client.once()
-    cloudwatch_client = boto3.client('cloudwatch', region_name=config.client['aws_region'])
-    print(put_cw_data(cloudwatch_client, client.get_data()))
+    boto_client = boto3.client('cloudwatch', region_name=config.client['aws_region'])
+    print(client.once(put_cw_data, boto_client))
 
+
+@query.command()
+@click.option("-w", "--wait-time", help="Time to wait between polling in seconds. Default 300", default=300)
+def poll(wait_time):
+    """
+    Repeatedly SNMP poll and send data to cloudwatch, sleeping --wait-time seconds.
+    :param wait_time:
+    :return:
+    """
+    print(f"Snmp CloudWatch starting in polling mode; sleeping {wait_time}seconds between polls.")
+    config = Config("sample.config")
+    client = Client(config)
+    boto_client = boto3.client('cloudwatch', region_name=config.client['aws_region'])
+    try:
+        client.poll(put_cw_data, boto_client, wait_time)
+    except KeyboardInterrupt:
+        print("Exiting polling")
 
 
 def main():
     query()
+
 
 if __name__ == '__main__':
     main()
